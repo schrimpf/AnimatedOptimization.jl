84e58bf1f62d18d799fccf9d8e042c474b0ffb3a
Paul Schrimpf <paul.schrimpf@gmail.com>
2019-10-18 23:54:35 -0700

    Updated documentation.

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ Some optimization algorithms with animations for functions from R² → R.
 These are meant for teaching. They have not been extenisvely tested
 and are likely not well-suited for other uses. 
 
-For examples, see [these notes.](https://schrimpf.github.io/AnimatedOptimization/optimization)
+For examples, see [these notes.](https://schrimpf.github.io/AnimatedOptimization.jl/optimization)
 
 <!-- [![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://schrimpf.github.io/AnimatedOptimization.jl/stable) -->
-[![Docs](https://img.shields.io/badge/docs-dev-blue.svg)](https://schrimpf.github.io/AnimatedOptimization) 
+[![Docs](https://img.shields.io/badge/docs-dev-blue.svg)](https://schrimpf.github.io/AnimatedOptimization.jl) 
 <!-- [![Build Status](https://travis-ci.com/schrimpf/AnimatedOptimization.jl.svg?branch=master)](https://travis-ci.com/schrimpf/AnimatedOptimization.jl) -->
--- docs/make.jl
+++ docs/make.jl
@@ -1,21 +1,24 @@
 using Documenter, AnimatedOptimization, DocumenterMarkdown
-runweave=false
+runweave=true
 if runweave
   using Weave
 
-  cd("jmd")
+  cd("build")
   try
-    weave("optimization.jmd",out_path="../build", cache=:user, cache_path="../weavecache",
-          doctype="github", mod=Main,
-          pandoc_options=["--toc","--toc-depth=2","--filter=pandoc-citeproc"])
-    #notebook("optimization.jmd", nbconvert_options="--allow-errors")
+    weave("../jmd/optimization.jmd",out_path="../build", cache=:user,
+          cache_path="../weavecache", doctype="github", mod=Main,
+          args=Dict("md" => "true"))
+    #notebook("../jmd/optimization.jmd", out_path="../build",
+    #         nbconvert_options="--allow-errors")
+    #convert_doc("../jmd/optimization.jmd", "../build/optimization.ipynb")
+    #weave("../jmd/optimization.jmd",out_path="../build", cache=:user,
+    #      cache_path="../weavecache", doctype="github", mod=Main,
+    #      args=Dict("notebook" => "false"))    
   finally 
     cd("..")
   end
-  #run(`pandoc jmd/optimization.md -o build/optimization.md -f markdown -t gfm`)
-  #cp() # copy gifs to build
-  if (isfile("src/temp.md"))
-    rm("src/temp.md")
+  if (isfile("build/temp.md"))
+    rm("build/temp.md")
   end
 end
 
@@ -26,17 +29,17 @@ makedocs(
   pages=[
     "Home" => "index.md"
   ],
-  repo="https://github.com/schrimpf/AnimatedOptimization/blob/{commit}{path}#L{line}",
+  repo="https://github.com/schrimpf/AnimatedOptimization.jl/blob/{commit}{path}#L{line}",
   sitename="AnimatedOptimization.jl",
   authors="Paul Schrimpf <paul.schrimpf@gmail.com>"
 )
 
 run(`mkdocs build`)
 
-deploy=false
+deploy=true
 if deploy || "deploy" in ARGS
-
-  include("faketravis.jl")
+  
+  #include("faketravis.jl")
 
   # deploydocs(deps = nothing, make = nothing,
   #            repo = "github.com/schrimpf/AnimatedOptimization",
@@ -45,15 +48,16 @@ if deploy || "deploy" in ARGS
   #            devbranch = "master"
   #            )
 
-  deploydocs(
-    repo = "github.com/schrimpf/AnimatedOptimization",
-    deps   = Deps.pip("mkdocs", "pygments", "python-markdown-math", "bootswatch"),
-    make   = () -> run(`mkdocs build`),
-    target = "site",
-    branch = "gh-pages",
-    devbranch = "master"
-  )
-
+  #deploydocs(
+  #  repo = "github.com/schrimpf/AnimatedOptimization",
+  #  deps   = Deps.pip("mkdocs", "pygments", "python-markdown-math", "bootswatch"),
+  # make   = () -> run(`mkdocs build`),
+  # target = "site",
+  #  branch = "gh-pages",
+  #  devbranch = "master"
+  #)
+  run(`mkdocs gh-deploy`)
+  
 end
 
 
